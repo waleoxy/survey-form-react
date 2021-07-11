bdfab0c08b7adaa82fc069a94b2a3269d9da39ad
Olawale Otubu <waleoxy@gmail.com>
2021-07-11 15:16:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import { useState, useEffect } from 'react';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const API_URL = "https://opentdb.com/api.php?amount=10&category=28&type=multiple";
 
 function App() {
+
+  const [question, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      setQuestions(data.results);
+    });
+  }, [])
+  console.log(question)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    Hellooooooo from App
+    <Footer/>
+    </>
   );
 }
 
